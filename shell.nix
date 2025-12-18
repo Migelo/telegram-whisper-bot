9076286772dda132c91686616d7631a47c3e6d93
Migelo <miha@filetki.si>
2025-12-18 15:14:16 +0000

    simplify shell.nix

--- shell.nix
+++ shell.nix
@@ -2,16 +2,10 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-
-    pre-commit
-    
-    # System dependencies
-    ffmpeg  # Required for whisper
-    
-    # Python 3.12 and packages
-    python312
-    python312Packages.python-telegram-bot
-    python312Packages.openai-whisper
+    ffmpeg
+    (python312.withPackages (ps: with ps; [
+      python-telegram-bot
+      openai-whisper
+    ]))
   ];
-
 }
